1cc26d2df49e4eb747167958cf422db894926062
stevenfavre <47446557+stevenfavre@users.noreply.github.com>
2022-09-02 11:44:36 +0200

    Base des fichiers
    Mise à jour du dossier avec les bonnes informations et les bonnes extensions. Include du footer ainsi que la barre de navigation a été réalisé...

---
+++ Site/match.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>Services - Brand</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+  <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+</head>
+
+<body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
+  <?php include_once('default_pages/navbar.php'); ?>
+  <section class="py-5">
+    <div class="container py-5">
+      <div class="row mb-4 mb-lg-5">
+        <div class="col-md-8 col-xl-6 text-center mx-auto">
+          <p class="fw-bold text-success mb-2">Liste des matchs</p>
+          <h3 class="fw-bold">Match du tournoi :&nbsp;</h3>
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-2 mx-auto" style="max-width: 900px;">
+        <div class="col d-xl-flex justify-content-xl-center align-items-xl-center mb-5">
+          <h5 class="fw-bold text-center">Score : 0 - 0</h5>
+        </div>
+        <div class="col d-md-flex align-items-md-end align-items-lg-center mb-5">
+          <div>
+            <h5 class="fw-bold">Team B - Team D</h5>
+            <p class="text-muted mb-4">Lieu :&nbsp;<br>Date :&nbsp;<br></p>
+          </div>
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-2 mx-auto" style="max-width: 900px;">
+        <div class="col text-center d-xl-flex order-md-last justify-content-xl-center align-items-xl-center mb-5">
+          <h5 class="fw-bold text-center">Score : 0 - 0</h5>
+        </div>
+        <div class="col d-md-flex align-items-md-end align-items-lg-center mb-5">
+          <div>
+            <h5 class="fw-bold">Team A - Team B</h5>
+            <p class="text-muted mb-4">Lieu :&nbsp;<br>Date :&nbsp;<br></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php include_once('default_pages/footer.php'); ?>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
---
+++ Site/tournois.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>Projects - Brand</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+  <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+</head>
+
+<body>
+  <?php include_once('default_pages/navbar.php'); ?>
+  <section class="py-5">
+    <div class="container py-5">
+      <div class="row mb-5">
+        <div class="col-md-8 col-xl-6 text-center mx-auto">
+          <h2 class="fw-bold">Tournois</h2>
+          <p class="text-muted w-lg-50">Différents tournois géré grâce à notre système.&nbsp;</p>
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-2 mx-auto" style="max-width: 900px;">
+        <div class="col mb-4">
+          <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="assets/img/products/2173435-silhouette-joueur-de-football-tir-rapide-un-ballon-sur-un-fond-blanc-illustration-vectoriel.jpg" style="height: 250px;"></a>
+            <div class="py-4">
+              <h4 class="fw-bold">Tournoi du 20.08.22</h4>
+              <p class="text-muted">Ce tournoi est organisé à [nom_salle], il comprend ces différentes équipes. [noms_équipes]</p>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="assets/img/products/2173435-silhouette-joueur-de-football-tir-rapide-un-ballon-sur-un-fond-blanc-illustration-vectoriel.jpg" style="height: 250px;"></a>
+            <div class="py-4">
+              <h4 class="fw-bold">Tournoi du 06.08.22</h4>
+              <p class="text-muted">Ce tournoi est organisé à [nom_salle], il comprend ces différentes équipes. [noms_équipes]<br></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php include_once('default_pages/footer.php'); ?>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
