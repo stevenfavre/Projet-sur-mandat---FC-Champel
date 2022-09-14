5bd8c90cbafcd1378b101425a0407957306a1531
sofian971203 <106809628+sofian971203@users.noreply.github.com>
2022-09-15 00:30:55 +0200

    Add files via upload
    Ajout d'un nouveau formulaire

---
+++ Site/Site/affichage_club_actif.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once('./functions/dbconnection.php');
+require_once('./functions/Fonctions_Sofian.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Contacts - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+</head>
+
+<body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
+    <?php include_once('default_pages/navbar.php'); ?>
+    <section class="py-5">
+        <div class="container py-5">
+            <div class="row mb-5">
+                <div class="col-md-8 col-xl-6 text-center mx-auto">
+                    <p class="fw-bold text-success mb-2"></p>
+                    <h2 class="fw-bold"></h2>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-6 col-xl-4">
+                <h2 class="fw-bold">Liste des clubs</h2>
+                </div>
+                <?php afficher_ClubActif() ?>
+            </div>
+            <br /><br />
+            <a href="../Site/Formulaire_suppression_club.php">Retour à la page de suppression d'un club</a>
+            <br /><br />
+            <a href="../Site/inscription_tournoi.php">Retour à la page d'inscription au tournoi</a>
+        </div>
+    </section>
+    <?php include_once('default_pages/footer.php'); ?>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
