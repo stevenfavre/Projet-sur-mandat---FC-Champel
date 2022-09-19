6321506d1bdf7dd9c0964da3d191cb58f9199ddb
daytnclp98 <106809590+daytnclp98@users.noreply.github.com>
2022-09-19 18:00:17 +0200

    Add files via upload
    Corrections des formulaires

---
+++ Site_V2/Site/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <title>Home - Brand</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+  <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+</head>
+
+<body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
+  <?php include_once('default_pages/navbar.php'); ?>
+  <header class="bg-primary-gradient">
+    <div class="container pt-4 pt-xl-5">
+      <div class="row pt-5">
+        <div class="col-md-8 col-xl-6 text-center text-md-start mx-auto">
+          <div class="text-center">
+            <p class="fw-bold text-success mb-2">#1 dans l'organisation de tournoi</p>
+            <h1 class="fw-bold">La meilleure solution pour l'organisation d'un tournoi.</h1>
+          </div>
+        </div>
+        <div class="col-12 col-lg-10 mx-auto">
+          <div class="position-relative" style="display: flex;flex-wrap: wrap;justify-content: flex-end;">
+            <div style="position: relative;flex: 0 0 45%;transform: translate3d(-15%, 35%, 0);"><img class="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.8" src="assets/img/products/2173435-silhouette-joueur-de-football-tir-rapide-un-ballon-sur-un-fond-blanc-illustration-vectoriel.jpg"></div>
+            <div style="position: relative;flex: 0 0 45%;transform: translate3d(-5%, 20%, 0);"><img class="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.4" src="assets/img/products/fond-tournoi-football-elegant-vague-verte_1017-13302.jpg"></div>
+            <div style="position: relative;flex: 0 0 60%;transform: translate3d(0, 0%, 0);"><img class="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.25" src="assets/img/products/ahp0019-alpa-107879-C.jpg"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <section class="py-5"></section>
+  <section>
+    <div class="container bg-primary-gradient py-5">
+      <div class="row">
+        <div class="col-md-8 col-xl-6 text-center mx-auto">
+          <p class="fw-bold text-success mb-2">Nos services</p>
+          <h3 class="fw-bold">Ce que nous pouvons vous apporter</h3>
+        </div>
+      </div>
+      <div class="py-5 p-lg-5">
+        <div class="row row-cols-1 row-cols-md-2 mx-auto" style="max-width: 900px;">
+          <div class="col mb-5">
+            <div class="card shadow-sm">
+              <div class="card-body px-4 py-5 px-md-5">
+                <div class="bs-icon-lg d-flex justify-content-center align-items-center mb-3 bs-icon" style="top: 1rem;right: 1rem;position: absolute;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-bell">
+                    <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"></path>
+                  </svg></div>
+                <h5 class="fw-bold card-title">Système de notification</h5>
+                <p class="text-muted card-text mb-4">L'administrateur aura la possibilité de voir des notifications pour les différentes demande d'inscriptions aux tournois.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col mb-5">
+            <div class="card shadow-sm">
+              <div class="card-body px-4 py-5 px-md-5">
+                <div class="bs-icon-lg d-flex justify-content-center align-items-center mb-3 bs-icon" style="top: 1rem;right: 1rem;position: absolute;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-bezier">
+                    <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
+                    <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
+                  </svg></div>
+                <h5 class="fw-bold card-title">Algorithme d'organisation</h5>
+                <p class="text-muted card-text mb-4">Notre site est capable de réaliser l'organisation de vos différents matchs. Les qualifications seront automatisées.&nbsp;</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section></section>
+  <?php include_once('default_pages/footer.php'); ?>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
