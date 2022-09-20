3161c679d3f7d983e7cddf31f49c8423b6ebf51b
daytnclp98 <106809590+daytnclp98@users.noreply.github.com>
2022-09-20 18:36:02 +0200

    Add files via upload
    Nouvelles modifications

--- Site_V2/modifier_tournoi.php
+++ Site_V2/modifier_tournoi.php
@@ -3,9 +3,6 @@
   include('./functions/tournoi.php');
 
 
-
-  //echo $_POST['submit'];
-
   if (isset($_POST['submit'])) {
 
     $IdTournoi = $_POST['ID_Tournoi'];
@@ -15,7 +12,6 @@
     update_tournoi($IdTournoi, $Date_debut, $Date_fin, $Fk_ID_Salle);
   }
   ?>
-
  <!DOCTYPE html>
  <html lang="en">
 
@@ -32,97 +28,116 @@
  <body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
    <?php include_once('default_pages/navbar.php'); ?>
 
-   <section class="py-5"></section>
-   <section>
-
+   <section class="py-5">
      <div class="container bg-primary-gradient py-5">
        <div class="row">
          <div class="col-md-8 col-xl-6 text-center mx-auto">
-
            <h2 class="fw-bold">Modification d'un tournoi</h2>
          </div>
        </div>
-
-       <!--      <div class="py-5 p-lg-5">
-         <div class="row row-cols-1 row-cols-md-2 mx-auto" style="max-width: 900px;">
-           <div class="col mb-5">
-             <div class="card shadow-sm">
-
-             </div>
-           </div>
-         </div>
-         <div class="col mb-5"> -->
-
        <div class="card shadow-sm">
          <div class="card-body px-4 py-5 px-md-5">
-           <!-- <div class="bs-icon-lg d-flex justify-content-center align-items-center mb-3 bs-icon" style="top: 1rem;right: 1rem;position: absolute;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-bezier"> -->
-           <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
-           <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
+         </div>
 
-           <table>
-             <p class="fw-bold text-success mb-2">Tous les tournois</p>
-             <tbody>
-               <!-- <i class="fa-solid fa-magnifying-glass"></i> -->
-               <tr>
-                 <td>
-                   <ul>
-                     <h5 class="fw-bold card-title">Date début</h5>
-                   </ul>
+         <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
+         <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
+         <table>
+           <tbody>
+             <tr>
+               <td>
+                 <ul>
+                   <h5 class="fw-bold card-title"></h5>
+                 </ul>
+               </td>
+               <td>
+                 <ul>
+                   <h5 class="fw-bold card-title"> </h5>
+                 </ul>
+               </td>
+               <td>
+               <td>
+                 <ul>
+                   <h5 class="fw-bold card-title"> </h5>
+                 </ul>
+               </td>
+               <ul>
+                 <h5 class="fw-bold card-title"></h5>
+                 <?php afficher_date_tournoi(); ?>
                  </td>
-                 <td>
-                   <ul>
-                     <h5 class="fw-bold card-title">Date fin </h5>
+             </tr>
+           </tbody>
+         </table>
+         <table>
+           <tbody>
+             <tr>
+               <td>
+                 <ul>
+                   <h5 class="fw-bold card-title"> </h5>
+                 </ul>
+               </td>
+               <td>
+               <td>
+                 <ul>
+                   <h5 class="fw-bold card-title"> </h5>
+                 </ul>
+               </td>
+               <ul>
+                 <form action="modifier_tournoi.php" class="p-3 p-xl-4" method="post">
+                   <h3 class="fw-bold text-success mb-2">Modifier tournoi</h3>
 
-                   </ul>
-                 </td>
-                 <td>
-                   <ul>
-                     <h5 class="fw-bold card-title">Salle</h5>
-                     <?php afficher_infos_tournois(); ?>
-                   </ul>
-                 </td>
-               </tr>
-             </tbody>
-           </table>
-
-           <table>
-             <h5 class="fw-bold card-title">Modifier un tournoi</h5>
-             <tbody>
-               <tr>
-                 <td>
-                   <form action="modifier_tournoi.php" class="p-3 p-xl-4" method="post">
-                     <p class="fw-bold text-success mb-2">Choisissez la date du tournoi que vous souhaitez modifier</p>
-                     <select name="ID_Tournoi" id="listeIdTournoi">
+                   <h5 class="fw-bold card-title\">Sélectionnez le tournoi que vous souhaitez modifier
+                     <select name="ID_Tournoi" id="listeIdTournoi" onchange="afficheDetails()">
                        <?php selection_tournoi(); ?>
                      </select>
-                     <p class="fw-bold text-success mb-2">Modifier la date du tournoi</p>
-                     <label for="text">Date de début:</label>
-                     <div class="mb-3"><input class="form-control" type="date" id="date-1" name="Date_Debut_Tournoi" placeholder="Date de début"></div>
-                     <label for="text">Date de fin:</label>
-                     <div class="mb-3"><input class="form-control" type="date" id="date-2" name="Date_Fin_Tournoi" placeholder="Date de fin"></div>
-                     <p class="fw-bold text-success mb-2">Modifier la salle du tournoi</p>
-                     <label for="text">Salle Tournoi:</label>
-                     <select name="ID_Salle" id="listeIdTournoi">
-                       <?php selection_salle_tournoi(); ?>
-                     </select>
-                     <br /><br />
-                     <div><button class="btn btn-primary" type="submit" name="submit">Modifier tournoi</button></div>
-                     <br /><br />
-                     <div><button class="btn btn-primary" type="reset" name="reset">Annuler</button></div>
-                   </form>
-
+                   </h5>
+                   <h5 class="fw-bold text-success mb-2">Modifier la date du tournoi</h5>
+                   <label for="text">Date de début:<div class="mb-3"><input class="form-control" type="date" name="Date_Debut_Tournoi" placeholder="Date de début" id="dateDebut"></div></label>
+                   <label for="text">Date de fin:<div class="mb-3"><input class="form-control" type="date" name="Date_Fin_Tournoi" placeholder="Date de fin" id="dateFin"></div></label>
+                   <h5 class="fw-bold text-success mb-2">Modifier la salle du tournoi</h5>
+                   <label for="text">Salle Tournoi:
+                     <select name="ID_Salle" id="listeIdSalle">
+                       <?php selection_salle_tournoi(); ?></select></label>
+                   </br></br>
+                   <button class="btn btn-primary" type="submit" name="submit">Modifier</button>
+                   <button class="btn btn-primary" type="reset" name="reset">Annuler</button>
+                   </br></br>
+               </ul>
+               </td>
+             </tr>
+           </tbody>
+           </form>
+         </table>
+       </div>
+     </div>
    </section>
-   </td>
-   </tr>
-   </tbody>
-   </table>
-
-
-
    <?php include_once('default_pages/footer.php'); ?>
    <script src="assets/js/jquery.min.js"></script>
    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
    <script src="assets/js/script.min.js"></script>
  </body>
+
+ <script>
+   //aidée par Sebastien
+   let inputDateDebut = $("#dateDebut")
+   let inputDateFin = $("#dateFin")
+   let selectSalle = $("#listeIdSalle")
+   let selectDate = $("#listeIdTournoi")
+
+   let listeTournois = {
+     <?php
+      $tournois = afficher_Tournoi();
+      foreach ($tournois as $element) {
+        echo "'" . $element["ID_Tournoi"] . "' : Array('" . $element["Date_Debut_Tournoi"] . "', '" . $element["Date_Fin_Tournoi"] . "', '" . $element["ID_Salle"] . "'),\n";
+      }
+      ?>
+   }
+
+   function afficheDetails() {
+     let selectValue = selectDate.val()
+     inputDateDebut.val(listeTournois[selectValue][0])
+     inputDateFin.val(listeTournois[selectValue][1])
+     selectSalle.val(listeTournois[selectValue][2])
+   }
+ </script>
 
  </html>
--- Site_V2/supprimer_tournoi.php
+++ Site_V2/supprimer_tournoi.php
@@ -3,19 +3,22 @@
   include('./functions/tournoi.php');
 
 
-
-
+  $submit = null;
   if (isset($_POST['submit'])) {
-    $IdTournoi = $_POST['ID_Tournoi'];
-    update_suppresion_logique($IdTournoi);
+    $submit = $_POST['submit'];
+  }
+  if ($submit == "modifier") {
+    update_activer_logique();
   }
   ?>
+
 
  <!DOCTYPE html>
  <html lang="en">
 
  <head>
    <meta charset="utf-8">
+   <script src="https://kit.fontawesome.com/5a023d1c0f.js" crossorigin="anonymous"></script>
    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
    <title>Annulation - Tournoi</title>
    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
@@ -31,75 +34,68 @@
      <div class="container bg-primary-gradient py-5">
        <div class="row">
          <div class="col-md-8 col-xl-6 text-center mx-auto">
-           <p class="fw-bold text-success mb-2">Annuler un tournoi</p>
            <h2 class="fw-bold">Annulation d'un tournoi</h2>
          </div>
        </div>
-
-       <!--        <div class="py-5 p-lg-5">
-         <div class="row row-cols-1 row-cols-md-2 mx-auto" style="max-width: 900px;">
-           <div class="col mb-5">
-             <div class="card shadow-sm">
-
-             </div>
-           </div>
-         </div>
-         <div class="col mb-5"> -->
-
        <div class="card shadow-sm">
          <div class="card-body px-4 py-5 px-md-5">
-           <!-- <div class="bs-icon-lg d-flex justify-content-center align-items-center mb-3 bs-icon" style="top: 1rem;right: 1rem;position: absolute;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-bezier"> -->
-           <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
-           <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
+         </div>
+         <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
+         <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
+         <table>
+           <tbody>
+             <tr>
+               <td>
+                 <ul>
+                   <h5 class="fw-bold card-title"></h5>
+                   <?php afficher_date_tournoi(); ?>
+                 </ul>
+               </td>
+             </tr>
+           </tbody>
+         </table>
 
-           <table>
-             <p class="fw-bold text-success mb-2">Tous les tournois</p>
-             <tbody>
-               <tr>
-                 <td>
-                   <ul>
-                     <h5 class="fw-bold card-title">Date début</h5>
-                   </ul>
-                 </td>
-                 <td>
-                   <ul>
-                     <h5 class="fw-bold card-title">Date fin </h5>
+         <table>
+           <tbody>
+             <tr>
+               <td>
+                 <!--  <ul>
+                   <h5 class="fw-bold card-title"> </h5>
+                 </ul> -->
+               </td>
+               <td>
+               <td>
+                 <ul>
+                   <h5 class="fw-bold card-title"> </h5>
+                 </ul>
+               </td>
 
-                   </ul>
-                 </td>
-                 <td>
-                   <ul>
-                     <h5 class="fw-bold card-title">Salle</h5>
-                     <?php afficher_infos_tournois(); ?>
-                   </ul>
-                 </td>
-               </tr>
-             </tbody>
-           </table>
-
-
-
-
-           <table>
-             <h5 class="fw-bold card-title">Supprimer un tournoi</h5>
-             <tbody>
-               <tr>
-                 <td>
-                   <form action="supprimer_tournoi.php" class="p-3 p-xl-4" method="post">
-                     <p class="fw-bold text-success mb-2">Choisissez la date du tournoi que vous souhaitez annuler</p>
+               <ul>
+                 <form action="supprimer_tournoi.php" class="p-3 p-xl-4" method="post">
+                   <h3 class="fw-bold text-success mb-2">Annuler tournoi</h3>
+                   <h5 class="fw-bold card-title\">Choisissez la date du tournoi que vous souhaitez annuler ou activer
                      <select name="ID_Tournoi" id="listeIdTournoi">
-                       <?php selection_tournoi(); ?>
+                       <?php selection_tournoi();
+                        if ($submit == "supprimer") {
+                          update_suppresion_logique();
+                        } ?>
                      </select>
-                     <br /><br />
-                     <div><button class="btn btn-primary shadow d-block w-100" type="submit" name="submit">Supprimer tournoi</button></div>
-                     <br /><br />
-                     <div><button class="btn btn-primary shadow d-block w-100" type="reset" name="reset">Annuler</button></div>
-                   </form>
+                   </h5>
+                   <br /><br />
+                   <button class="btn btn-primary" type="submit" name="submit" value="supprimer">Supprimer</button>
+                   <button class="btn btn-primary" type="submit" name="submit" value="modifier">Activer</button>
+                   <button class="btn btn-primary" type="reset" name="reset">Annuler</button>
+                   <a href="afficher_tournois.php">Revenir</a>
+               </ul>
+
+               </td>
+             </tr>
+           </tbody>
+           </form>
+         </table>
+       </div>
+     </div>
    </section>
-   </td>
-   </tr>
-   </tbody>
-   </table>
 
    <?php include_once('default_pages/footer.php'); ?>
    <script src="assets/js/jquery.min.js"></script>
