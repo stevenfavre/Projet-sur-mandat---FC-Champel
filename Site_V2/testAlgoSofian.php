ed78e30443f83389173b2320e0f31fbb2d7d4cfe
sofian971203 <106809628+sofian971203@users.noreply.github.com>
2022-09-23 16:11:38 +0200

    Add files via upload

---
+++ Site_V2/testAlgoSofian.php
@@ -0,0 +1,9 @@
+<?php
+require_once('./functions/dbconnection.php'); //Fait appel à la page se trouve la connexion à la BDD.
+require_once('./functions/Fonctions_Sofian.php'); //Fait appel à la page où se trouvent les fonction 
+require_once('./functions/debug.php');
+require_once('./functions/steven_fonctions.php');
+require_once('./functions/algorithme_match_eliminationDirecte.php');
+
+
+?>
