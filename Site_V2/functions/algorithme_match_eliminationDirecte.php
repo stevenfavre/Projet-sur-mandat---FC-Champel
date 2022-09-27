73d6e37f156ebb118176c46ea106a4bb91464693
sofian971203 <106809628+sofian971203@users.noreply.github.com>
2022-09-27 20:53:18 +0200

    Add files via upload

--- Site_V2/functions/algorithme_match_eliminationDirecte.php
+++ Site_V2/functions/algorithme_match_eliminationDirecte.php
@@ -13,39 +13,66 @@ define("NB_FINALE", 1);
 define("NB_MATCH_3_PLACE", 1);
 define("NB_MATCH_5_8_PLACE", 4);
 
-//Définition de 4 listes de 4 équipes 
-$GroupeA = array();
-$GroupeB = array();
-$GroupeC = array();
-$GroupeD = array();
 
-function creerQuartFinale($GroupeA, $GroupeB, $GroupeC, $GroupeD)
+$id_tournoi = $_GET["id_tournoi"];
+createGroupe($id_tournoi);
+
+// Fonction répartissant les équipes dans les différents groupes
+function createGroupe($fk_id_tournoi)
 {
-    $bdd = connectDB();
-    $sqlQuery = 'select * from  equipe';
-    $req = $bdd->prepare($sqlQuery);
-    $req->execute([]);
-    $count = $req->rowCount();
-    $equipes = $req->fetchAll();
-    foreach ($equipes as $equipe) {
-        echo $equipe;
+    $redo = false;
+    //$_SESSION['Groupes'] = array();
+    $_SESSION['Equipes'] = array();
+
+    // Groupe stockant les équipes
+    $GroupeUn = array();
+    $GroupeDeux = array();
+    $GroupeTrois = array();
+    $GroupeQuatre = array();
+
+    foreach (getInscriptions($fk_id_tournoi, "Validé") as $equipes) {
+        // Récupération des équipes individuellement
+        $equipe = selectEquipeWithID($equipes['FK_ID_Equipe']);
+        // Stockage des équipes dans une liste de session
+        array_push($_SESSION['Equipes'], $equipe);
+    }
+
+    if (count($_SESSION['Equipes']) >= NB_EQUIPES_IDEAL) {
+
+        shuffle($_SESSION['Equipes']);
+
+        // https://stackoverflow.com/questions/23362451/php-generate-unique-teams-using-number-combination
+        $division = array_chunk($_SESSION['Equipes'], NB_EQUIPES_IDEAL);
+
+        foreach ($division[0] as $equipe) {
+            array_push($GroupeUn, $equipe);
+            updateGroupe($equipe[0]['ID_Equipe'], 1);
+        }
+
+        foreach ($division[1] as $equipe) {
+            array_push($GroupeDeux, $equipe);
+            updateGroupe($equipe[0]['ID_Equipe'], 2);
+        }
+
+        foreach ($division[2] as $equipe) {
+            array_push($GroupeTrois, $equipe);
+            updateGroupe($equipe[0]['ID_Equipe'], 3);
+        }
+
+        foreach ($division[3] as $equipe) {
+            array_push($GroupeQuatre, $equipe);
+            updateGroupe($equipe[0]['ID_Equipe'], 4);
+        }
+
     }
 }
 
+function createQuartFinale($GroupeUn, $GroupeDeux, $GroupeTrois, $GroupeQuatre){
 
-function creerDemiFinale()
-{
-}
+    echo $GroupeUn[0] . " vs " . $GroupeQuatre[1];
+    echo $GroupeUn[1] . " vs " . $GroupeQuatre[0];
+    echo $GroupeDeux[0] . " vs " . $GroupeTrois[1];
+    echo $GroupeDeux[1] . " vs " . $GroupeTrois[0];
+    
 
-
-function creerFinale()
-{
-}
-
-function creerMatch3Place()
-{
-}
-
-function creerMatch5A8Place()
-{
 }
