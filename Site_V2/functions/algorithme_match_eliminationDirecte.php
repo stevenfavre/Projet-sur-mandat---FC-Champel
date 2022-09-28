b685c9bd3770eb1a0bd631b8a968826945b69914
sofian971203 <106809628+sofian971203@users.noreply.github.com>
2022-09-28 21:44:50 +0200

    Modifications Sofian
    Continuation de l'algorithme

--- Site_V2/functions/algorithme_match_eliminationDirecte.php
+++ Site_V2/functions/algorithme_match_eliminationDirecte.php
@@ -3,7 +3,7 @@ require_once('dbconnection.php'); //Fait appel à la page se trouve la connexion
 require_once('Fonctions_Sofian.php'); //Fait appel à la page où se trouvent les fonction 
 require_once('debug.php');
 require_once('steven_fonctions.php');
-require_once('algorithme_groupe.php');
+//require_once('algorithme_groupe.php');
 
 
 // Définition de la constant concernant le nombre d'équipe idéal au total dans le tournoi
@@ -14,21 +14,82 @@ define("NB_FINALE", 1);
 define("NB_MATCH_3_PLACE", 1);
 define("NB_MATCH_5_8_PLACE", 4);
 
-//Récupértion des groupes depuis l'algorithme
-$_SESSION['GroupeUn'];
-$_SESSION['GroupeDeux'];
-$_SESSION['GroupeTrois'];
-$_SESSION['GroupeQuatre'];
 
-function createQuartFinale()
+$groupeA = array (
+    array("A1",1),
+    array("A2",2),
+    array("A3",3),
+    array("A4",4),
+  );
+
+  $groupeB = array (
+    array("B1",5),
+    array("B2",6),
+    array("B3",7),
+    array("B4",8),
+  );
+
+  $groupeC = array (
+    array("C1",9),
+    array("C2",10),
+    array("B3",11),
+    array("B4",12),
+  );
+
+  $groupeD = array (
+    array("D1",13),
+    array("D2",14),
+    array("B3",15),
+    array("B4",16),
+  );
+
+function createQuartFinale($groupeA, $groupeB, $groupeC, $groupeD)
 {
+    $quartFinaleUn = array (
+        ($groupeA[0]),
+        ($groupeD[1]),
+      );
+
+      $quartFinaleDeux = array (
+        ($groupeB[0]),
+        ($groupeC[1]),
+      );
+
+      $quartFinaleTrois = array (
+        ($groupeC[0]),
+        ($groupeB[1]),
+      );
+
+      $quartFinaleQuart = array (
+        ($groupeD[0]),
+        ($groupeA[1]),
+      );
+
+     printMatchQuart($quartFinaleUn);
+     printMatchQuart($quartFinaleDeux);
+     printMatchQuart($quartFinaleTrois);
+     printMatchQuart($quartFinaleQuart);
 }
 
+ createQuartFinale($groupeA, $groupeB, $groupeC, $groupeD);
 
-$bdd = connectDB();
-$bdd->query("SET NAMES 'utf8'");
+ function printMatchQuart($match){
+   echo $match[0][0] . " (" . $match[0][1] . " )" . " VS " . $match[1][0] . " (" . $match[1][1] . " )<br />";
+ }
+
+ function createDemiFinale(){
+
+ }
+
+ function createFinale(){
+    
+ }
 
 
 
-$quartFinaleUn = $bdd->query("INSERT INTO `Matchs`(`ID_Match`, `Date_Match`, `Heure_Debut_Match`, `Heure_Fin_Match`, `Duree_Match`, `Type_Match`, `But_Local_Match`, `But_Visiteur_Match`, `FK_ID_Local`, `FK_ID_Visiteur`, `FK_ID_Groupe`, `FK_ID_Tournoi`, `FK_ID_Terrain`, `Actif_Match`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','Quart de finale','[value-7]','[value-8]','[value-9]','[value-10]','[value-11]','[value-12]','[value-13]','[value-14]')");
-$quartFinaleUn->setFetchMode(PDO::FETCH_BOTH);
+
+
+
+
+
+
