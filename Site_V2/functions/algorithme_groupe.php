27e5f2199a35b0215fd8e7536aced0fe461cc2e2
sofian971203 <106809628+sofian971203@users.noreply.github.com>
2022-09-29 16:15:00 +0200

    Modification Sofian
    Continuation de l'algorithme de création de match phase élimination directe

--- Site_V2/functions/algorithme_groupe.php
+++ Site_V2/functions/algorithme_groupe.php
@@ -3,6 +3,8 @@ require_once "steven_fonctions.php";
 require_once "dbconnection.php";
 require_once "debug.php";
 
+session_start();
+
 // Définition de la constant concernant le nombre d'équipe idéal dans un groupe
 define("NB_EQUIPES_IDEAL", 4);
 define("NB_GROUPE_PARFAIT", 4);
@@ -86,3 +88,74 @@ function updateGroupe($id_equipe, $id_groupe)
         debug();
     }
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Liste des matchs</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+</head>
+
+<body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
+    <?php include_once('default_pages/navbar.php'); ?>
+    <section class="py-5">
+        <div class="container py-5">
+            <div class="row mb-4 mb-lg-5">
+                <div class="col-md-8 col-xl-6 text-center mx-auto">
+                    <p class="fw-bold text-success mb-2">Liste des matchs</p>
+                    <h3 class="fw-bold">Match du tournoi :&nbsp;</h3>
+                    <p class="text-muted">&nbsp;<a href="modifier_equipe_tournoi.php?id_tournoi=<?php echo $_SESSION['id_tournoi']; ?>">Equipes inscrites</a></p>
+                    <!-- <a class="btn btn-primary shadow" role="button" href="creer_match.php?id_tournoi=<?php echo $_SESSION['id_tournoi']; ?>">Créer un match</a> -->
+                    <a class="btn btn-primary shadow" role="button" href="./functions/algorithme_groupe.php?id_tournoi=<?php echo $_SESSION['id_tournoi']; ?>">Créer les groupes</a>
+                    <br><br>
+                    <input type="text" id="recherche" onkeyup="recherche()" placeholder="Recherche..." title="Rechercher un match">
+                    <?php
+                    // afficherSalleEtDate($_GET['id_tournoi']);
+                    ?>
+                </div>
+            </div>
+            <div id="divContenu">
+                <form action="match.php" method="get">
+                    <?php
+                    afficherMatch($_SESSION['id_tournoi']);
+                    ?>
+                </form>
+            </div>
+        </div>
+    </section>
+    <?php include_once('default_pages/footer.php'); ?>
+    <!-- Site d'aide pour la réalisation du filtrage des équipes 
+  https://www.w3schools.com/howto/howto_js_filter_lists.asp -->
+    <script>
+        function recherche() {
+            var input, filter, div, div2, a, i, txtValue;
+            input = document.getElementById("recherche");
+            filter = input.value.toUpperCase();
+            div = document.getElementById("divContenu");
+            div2 = div.getElementsByClassName("equipe");
+            console.log(div2);
+            for (i = 0; i < div2.length; i++) {
+                a = div2[i].getElementsByTagName("h5")[0];
+                txtValue = a.textContent || a.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    div2[i].style.display = "";
+                } else {
+                    div2[i].style.display = "none";
+                }
+            }
+        }
+    </script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
--- Site_V2/functions/algorithme_match_eliminationDirecte.php
+++ Site_V2/functions/algorithme_match_eliminationDirecte.php
@@ -7,7 +7,6 @@ require_once('steven_fonctions.php');
 
 
 // Définition de la constant concernant le nombre d'équipe idéal au total dans le tournoi
-define("NB_EQUIPES_IDEAL", 16);
 define("NB_QUART_FINALE", 4);
 define("NB_DEMI_FINALE", 2);
 define("NB_FINALE", 1);
@@ -15,81 +14,138 @@ define("NB_MATCH_3_PLACE", 1);
 define("NB_MATCH_5_8_PLACE", 4);
 
 
-$groupeA = array (
-    array("A1",1),
-    array("A2",2),
-    array("A3",3),
-    array("A4",4),
-  );
-
-  $groupeB = array (
-    array("B1",5),
-    array("B2",6),
-    array("B3",7),
-    array("B4",8),
-  );
-
-  $groupeC = array (
-    array("C1",9),
-    array("C2",10),
-    array("B3",11),
-    array("B4",12),
-  );
-
-  $groupeD = array (
-    array("D1",13),
-    array("D2",14),
-    array("B3",15),
-    array("B4",16),
-  );
+$groupeA = $_SESSION['GroupeUn'];
+$groupeB = $_SESSION['GroupeDeux'];
+$groupeC = $_SESSION['GroupeTrois'];
+$groupeD = $_SESSION['GroupeQuatre'];
 
 function createQuartFinale($groupeA, $groupeB, $groupeC, $groupeD)
 {
-    $quartFinaleUn = array (
-        ($groupeA[0]),
-        ($groupeD[1]),
-      );
+  $quartFinaleUn = array(
+    ($groupeA[0]),
+    ($groupeD[1]),
+  );
 
-      $quartFinaleDeux = array (
-        ($groupeB[0]),
-        ($groupeC[1]),
-      );
+  $idEquipe1 = 0;
+  foreach ($groupeA[0] as $equipe) {
+    $idEquipe1 = $equipe['ID_Equipe'];
+  }
 
-      $quartFinaleTrois = array (
-        ($groupeC[0]),
-        ($groupeB[1]),
-      );
+  $idEquipe2 = 0;
+  foreach ($groupeD[1] as $equipe) {
+    $idEquipe2 = $equipe['ID_Equipe'];
+  }
 
-      $quartFinaleQuart = array (
-        ($groupeD[0]),
-        ($groupeA[1]),
-      );
+  $quartFinaleDeux = array(
+    ($groupeB[0]),
+    ($groupeC[1]),
+  );
 
-     printMatchQuart($quartFinaleUn);
-     printMatchQuart($quartFinaleDeux);
-     printMatchQuart($quartFinaleTrois);
-     printMatchQuart($quartFinaleQuart);
+  $idEquipe3 = 0;
+  foreach ($groupeB[0] as $equipe) {
+    $idEquipe3 = $equipe['ID_Equipe'];
+  }
+
+  $idEquipe4 = 0;
+  foreach ($groupeC[1] as $equipe) {
+    $idEquipe4 = $equipe['ID_Equipe'];
+  }
+
+
+  $quartFinaleTrois = array(
+    ($groupeC[0]),
+    ($groupeB[1]),
+  );
+
+  $idEquipe5 = 0;
+  foreach ($groupeC[0] as $equipe) {
+    $idEquipe5 = $equipe['ID_Equipe'];
+  }
+
+  $idEquipe6 = 0;
+  foreach ($groupeB[1] as $equipe) {
+    $idEquipe6 = $equipe['ID_Equipe'];
+  }
+
+
+  $quartFinaleQuatre = array(
+    ($groupeD[0]),
+    ($groupeA[1]),
+  );
+
+  $idEquipe7 = 0;
+  foreach ($groupeD[0] as $equipe) {
+    $idEquipe7 = $equipe['ID_Equipe'];
+  }
+
+  $idEquipe8 = 0;
+  foreach ($groupeA[1] as $equipe) {
+    $idEquipe8 = $equipe['ID_Equipe'];
+  }
+
+  /*$idGroupeA = 0;
+  foreach ($groupeA as $groupe) {
+    $idGroupeA = $groupe['ID_Equipe'];
+  }
+
+  $idGroupeB = 0;
+  foreach ($groupeB as $groupe) {
+    $idGroupeB = $groupe['ID_Equipe'];
+  }
+
+  $idGroupeC = 0;
+  foreach ($groupeC as $groupe) {
+    $idGroupeC = $groupe['ID_Equipe'];
+  }
+
+  $idGroupeD = 0;
+  foreach ($groupeD as $groupe) {
+    $idGroupeD = $groupe['ID_Equipe'];
+  }*/
+
+
+  print_r($quartFinaleUn);
+  echo "<br /><br />";
+  print_r($quartFinaleDeux);
+  echo "<br /><br />";
+  print_r($quartFinaleTrois);
+  echo "<br /><br />";
+  print_r($quartFinaleQuatre);
+
+  $bdd = connectDB();
+
+  $insertquartUn = $bdd->query("SET NAMES 'utf8'");
+  $insertquartUn = $bdd->query("INSERT INTO `matchs`(`Date_Match`, `Heure_Debut_Match`, `Heure_Fin_Match`, `Duree_Match`, `Type_Match`, `But_Local_Match`, `But_Visiteur_Match`, `FK_ID_Local`, `FK_ID_Visiteur`, `FK_ID_Groupe`, `FK_ID_Tournoi`, `FK_ID_Terrain`, `Actif_Match`) 
+  VALUES ('2022-09-24','10:00:00','10:11:00','11','Quart de finale','0','0','$idEquipe1','$idEquipe2','1','1','1','1')");
+
+  $insertquartDeux = $bdd->query("SET NAMES 'utf8'");
+  $insertquartDeux = $bdd->query("INSERT INTO `matchs`(`Date_Match`, `Heure_Debut_Match`, `Heure_Fin_Match`, `Duree_Match`, `Type_Match`, `But_Local_Match`, `But_Visiteur_Match`, `FK_ID_Local`, `FK_ID_Visiteur`, `FK_ID_Groupe`, `FK_ID_Tournoi`, `FK_ID_Terrain`, `Actif_Match`) 
+  VALUES ('2022-09-24','10:15:00','10:26:00','11','Quart de finale','0','0','$idEquipe3','$idEquipe4','1','1','1','1')");
+
+  $insertquartTrois = $bdd->query("SET NAMES 'utf8'");
+  $insertquartTrois = $bdd->query("INSERT INTO `matchs`(`Date_Match`, `Heure_Debut_Match`, `Heure_Fin_Match`, `Duree_Match`, `Type_Match`, `But_Local_Match`, `But_Visiteur_Match`, `FK_ID_Local`, `FK_ID_Visiteur`, `FK_ID_Groupe`, `FK_ID_Tournoi`, `FK_ID_Terrain`, `Actif_Match`) 
+  VALUES ('2022-09-24','10:30:00','10:31:00','11','Quart de finale','0','0','$idEquipe5','$idEquipe6','1','1','1','1')");
+
+  $insertquartQuatre = $bdd->query("SET NAMES 'utf8'");
+  $insertquartQuatre = $bdd->query("INSERT INTO `matchs`(`Date_Match`, `Heure_Debut_Match`, `Heure_Fin_Match`, `Duree_Match`, `Type_Match`, `But_Local_Match`, `But_Visiteur_Match`, `FK_ID_Local`, `FK_ID_Visiteur`, `FK_ID_Groupe`, `FK_ID_Tournoi`, `FK_ID_Terrain`, `Actif_Match`) 
+  VALUES ('2022-09-24','10:40:00','10:51:00','11','Quart de finale','0','0','$idEquipe7','$idEquipe8','1','1','1','1')");
 }
 
- createQuartFinale($groupeA, $groupeB, $groupeC, $groupeD);
-
- function printMatchQuart($match){
-   echo $match[0][0] . " (" . $match[0][1] . " )" . " VS " . $match[1][0] . " (" . $match[1][1] . " )<br />";
- }
-
- function createDemiFinale(){
-
- }
-
- function createFinale(){
-    
- }
+createQuartFinale($groupeA, $groupeB, $groupeC, $groupeD);
 
 
+function createDemiFinale()
+{
+}
 
+function createFinale()
+{
+}
 
+function createMatch5A8ePlace()
+{
+}
 
-
-
-
-
+function createMatch9A16ePlce()
+{
+}
