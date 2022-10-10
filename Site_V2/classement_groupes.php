e8662ad2fe1b9279bde7d33f02849fa031d8c0dd
sofian971203 <106809628+sofian971203@users.noreply.github.com>
2022-10-10 14:28:39 +0200

    Add files via upload

--- Site_V2/classement_groupes.php
+++ Site_V2/classement_groupes.php
@@ -60,7 +60,9 @@ if (!empty($_GET['submit'])) {
             <!--     <a class="btn btn-primary shadow" href="javascript:history.go(-1)">Revenir</a> -->
             <a class="btn btn-primary shadow" href="match.php">Revenir</a>
 
-            <a class="btn btn-primary shadow" role="button" href="./functions/algorithme_match_eliminationDirect.php'<?php $_SESSION['id_tournoi'] ?>'">Générer les quarts de finale</a>
+            <a class="btn btn-primary shadow" role="button" href="./functions/algorithme_quart_finale.php'<?php $_SESSION['id_tournoi'] ?>'">Générer les quarts de finale</a>
+
+            <a class="btn btn-primary shadow" role="button" href="./functions/algorithme_match_9e_16e_place.php'<?php $_SESSION['id_tournoi'] ?>'">Générer les matchs pour la 9ème à la 16ème place</a>
           </div>
         </div>
       </div>
