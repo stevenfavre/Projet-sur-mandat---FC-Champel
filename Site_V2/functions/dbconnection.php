3b40cea9bf6d8a5ab56792adc3b70d0db799640c
sofian971203 <106809628+sofian971203@users.noreply.github.com>
2022-10-12 16:33:46 +0200

    Add files via upload

--- Site_V2/functions/dbconnection.php
+++ Site_V2/functions/dbconnection.php
@@ -3,31 +3,13 @@
 
 // Fonction permettant de faire la connection à la base de donnée
 
-// function connectDB()
+function connectDB()
 
-// {
-//     $dbServer = "hhva.myd.infomaniak.com";
-//     $dbName = "hhva_t23_7_v2";
-//     $dbUser = "hhva_t23_7_v2";
-//     $dbPwd = "3StNYEXg43";
-
-//     static $bdd = null;
-
-//     if ($bdd === null) {
-//         $bdd = new PDO("mysql:host=$dbServer;dbname=$dbName;charset=utf8", $dbUser, $dbPwd);
-//         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     }
-//     return $bdd;
-// }
-
-
-
-function connectDB() //fonction qui permet de se connecter à la BDD
 {
-    $dbServer = "localhost";
+    $dbServer = "hhva.myd.infomaniak.com";
     $dbName = "hhva_t23_7_v2";
-    $dbUser = "root";
-    $dbPwd = "";
+    $dbUser = "hhva_t23_7_v2";
+    $dbPwd = "3StNYEXg43";
 
     static $bdd = null;
 
@@ -37,3 +19,21 @@ function connectDB() //fonction qui permet de se connecter à la BDD
     }
     return $bdd;
 }
+
+
+
+// function connectDB() //fonction qui permet de se connecter à la BDD
+// {
+//     $dbServer = "localhost";
+//     $dbName = "hhva_t23_7_v2";
+//     $dbUser = "root";
+//     $dbPwd = "";
+
+//     static $bdd = null;
+
+//     if ($bdd === null) {
+//         $bdd = new PDO("mysql:host=$dbServer;dbname=$dbName;charset=utf8", $dbUser, $dbPwd);
+//         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     }
+//     return $bdd;
+// }
