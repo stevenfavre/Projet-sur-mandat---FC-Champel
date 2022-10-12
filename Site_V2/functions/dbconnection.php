306f9b4dad8c1ab5e2fbc34f0d0140f6e515d0b7
sofian971203 <106809628+sofian971203@users.noreply.github.com>
2022-10-12 16:20:12 +0200

    Add files via upload

--- Site_V2/functions/dbconnection.php
+++ Site_V2/functions/dbconnection.php
@@ -3,29 +3,29 @@
 
 // Fonction permettant de faire la connection à la base de donnée
 
-function connectDB()
+// function connectDB()
 
-{
-    $dbServer = "hhva.myd.infomaniak.com";
-    $dbName = "hhva_t23_7_v2";
-    $dbUser = "hhva_t23_7_v2";
-    $dbPwd = "3StNYEXg43";
+// {
+//     $dbServer = "hhva.myd.infomaniak.com";
+//     $dbName = "hhva_t23_7_v2";
+//     $dbUser = "hhva_t23_7_v2";
+//     $dbPwd = "3StNYEXg43";
 
-    static $bdd = null;
+//     static $bdd = null;
 
-    if ($bdd === null) {
-        $bdd = new PDO("mysql:host=$dbServer;dbname=$dbName;charset=utf8", $dbUser, $dbPwd);
-        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    return $bdd;
-}
+//     if ($bdd === null) {
+//         $bdd = new PDO("mysql:host=$dbServer;dbname=$dbName;charset=utf8", $dbUser, $dbPwd);
+//         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     }
+//     return $bdd;
+// }
 
 
 
-/* function connectDB() //fonction qui permet de se connecter à la BDD
+function connectDB() //fonction qui permet de se connecter à la BDD
 {
     $dbServer = "localhost";
-    $dbName = "hhva_t23_7_v1";
+    $dbName = "hhva_t23_7_v2";
     $dbUser = "root";
     $dbPwd = "";
 
@@ -36,4 +36,4 @@ function connectDB()
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     return $bdd;
-} */
+}
